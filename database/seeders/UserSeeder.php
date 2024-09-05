a1cfebd8ea9276fbe88f6d6fc6369a8c670f836b
apple <muhammedjishad7034@gmail.com>
2024-09-05 11:07:21 +0530

    userdata

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,14 +10,14 @@ class UserSeeder extends Seeder
     public function run()
     {
         User::create([
-            'name' => 'Jhon Due',
+            'name' => 'Muhammed Jishad',
             'fk_department' => 1,
             'fk_designation' => 1,
             'phone_number' => '1234567890'
         ]);
 
         User::create([
-            'name' => 'Tommy Mark',
+            'name' => 'Muhammed',
             'fk_department' => 2,
             'fk_designation' => 2,
             'phone_number' => '0987654321'
